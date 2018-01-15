7effdceb65cc5d0c63f0ed834a39fa016e582c0c
fcschmidt <fcschimidt@gmail.com>
2018-01-14 23:35:12 -0200

    Create lib using for consumer api

---
+++ quotes_app/lib/consumer_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import requests
+
+
+def get_request(url):
+    """Request method"""
+    response = requests.get(url)
+    return response
+
+
+class ConsumerAPI:
+
+    def __init__(self):
+        # self.url = 'https://1c22eh3aj8.execute-api.us-east-1.amazonaws.com/challenge/quotes'
+        self.url = 'http://127.0.0.1:5000/challenge/quotes'
+        self.resp = None
+
+    def get_quotes(self):
+        self.resp = get_request(self.url).json()
+        return self.resp
+
+    def get_quote(self, number):
+        quote_url = self.url + '/' + '{}' .format(number)
+        self.resp = get_request(quote_url).json()
+        return self.resp
