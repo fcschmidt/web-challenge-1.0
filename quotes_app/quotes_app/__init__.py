fae0a35aed1e77b79d8a689718fb6ae300a18f0b
fcschmidt <fcschimidt@gmail.com>
2018-01-15 23:16:59 -0200

    Add methods using save Session and Using DB

--- quotes_app/quotes_app/__init__.py
+++ quotes_app/quotes_app/__init__.py
@@ -1,19 +1,25 @@
-from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+from pyramid.session import SignedCookieSessionFactory
+
+from .models.models import DBSession, Base
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+
+    engine = engine_from_config(settings, prefix='sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+    Base.metadata.create_all(engine)
+
+    session_factory = SignedCookieSessionFactory('itsaseekreet')
+    config.set_session_factory(session_factory)
+
     config.include('pyramid_jinja2')
-
-    config.add_route('home', '/')
-    config.add_route('quotes', '/quotes')
-    config.add_route('quote', '/quotes/{quote_number}')
-    config.add_route('random_quote', '/random')
-
+    config.include('.routes')
     config.scan('.views')
-    app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', 6543, app)
-    return server.serve_forever()
+
+    return config.make_wsgi_app()
