38372a86fe1dc2cce0cb8c892f0b9d6c01da57c8
fcschmidt <fcschimidt@gmail.com>
2018-01-16 15:19:23 -0200

    Create Inspection function

--- quotes_app/quotes_app/models/models.py
+++ quotes_app/quotes_app/models/models.py
@@ -4,9 +4,10 @@ from sqlalchemy import (
     Integer,
     Text,
     Date,
-    Time,
+    Time
     )
 
+from sqlalchemy import inspect
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import (
@@ -22,13 +23,19 @@ Base = declarative_base()
 
 
 class SessionLogModel(Base):
-    __tablename__ = 'sessions_access'
+    __tablename__ = 'SESSIONS'
     id = Column(Integer, primary_key=True)
-    session_uid = Column(Text)
-    session_url = Column(Text)
-    data = Column(Date, default=datetime.now().date())
+    uid_session = Column(Text, unique=True)
+    url_session = Column(Text)
+    date = Column(Date, default=datetime.now().date())
     time = Column(Time, default=datetime.now().time())
 
-    def __init__(self, session_uid, session_url):
-        self.session_uid = session_uid
-        self.session_url = session_url
+    def __init__(self, uid_session, url_session):
+        self.uid_session = uid_session
+        self.url_session = url_session
+
+    def object_to_dict(self):
+        dict_ = {
+            s.key: getattr(self, s.key) for s in inspect(self).mapper.column_attrs
+        }
+        return dict_
