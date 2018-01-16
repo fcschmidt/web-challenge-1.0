08e7a161e738346fc7e522fba7a6e3cb5d02684f
fcschmidt <fcschimidt@gmail.com>
2018-01-16 00:47:06 -0200

    Add Session Save

--- quotes_app/quotes_app/views.py
+++ quotes_app/quotes_app/views.py
@@ -1,6 +1,31 @@
-from lib.consumer_api import ConsumerAPI
-from pyramid.view import view_config, view_defaults
 import random
+import time
+import transaction
+from pyramid.view import view_config, view_defaults
+
+from lib.consumer_api import ConsumerAPI
+
+from .models.models import DBSession, SessionLogModel
+
+
+def uid_generator():
+    """Uid generator for a session and return it."""
+    timestamp = "%08x" % time.time()
+    uid_session = '%s%s' % (timestamp, "%016x" % random.getrandbits(128))
+    return uid_session
+
+
+def save_session(session, url):
+    """Save a session"""
+    if 'id' in session:
+        pass
+    else:
+        model = SessionLogModel(
+            uid_session=uid_generator(),
+            url_session=url,
+        )
+        DBSession.add(model)
+        transaction.commit()
 
 
 @view_defaults(route_name='home')
@@ -11,22 +36,27 @@ class QuoteViews:
 
     @view_config(renderer='templates/index.jinja2')
     def home(self):
+        save_session(self.request.session, self.request.current_route_url())
         return {'project_name': 'Web challenge 1.0'}
 
     @view_config(route_name='quotes', renderer='templates/quotes.jinja2')
     def get_quotes(self):
+        save_session(self.request.session, self.request.current_route_url())
         quotes = self.api.get_quotes()
         return quotes
 
     @view_config(route_name='quote', renderer='templates/quote.jinja2')
     def get_quote(self):
+        save_session(self.request.session, self.request.current_route_url())
         number = self.request.matchdict['quote_number']
         quote = self.api.get_quote(number)
         return {'quote_number': number, 'quote': quote['quote']}
 
     @view_config(route_name='random_quote', renderer='templates/random.jinja2')
     def get_random_quote(self):
+        save_session(self.request.session, self.request.current_route_url())
         size = len(self.api.get_quotes()['quotes'])
         random_number = random.randint(0, size - 1)
         quote = self.api.get_quote(random_number)
         return {'random_number': random_number, 'quote': quote['quote']}
+
