3515c697d414386696c19140948feb8a5cd14890
fcschmidt <fcschimidt@gmail.com>
2018-01-19 20:09:11 -0200

    Created a function that makes the query of sessions with the same uid.

--- quotes_app/quotes_app/api/api_sessions.py
+++ quotes_app/quotes_app/api/api_sessions.py
@@ -1,24 +1,41 @@
 import json
-from pyramid.response import Response
 from pyramid.view import view_config
-
-from util.json_util import default
-from builtins import IndexError
+from pyramid.response import Response
 
 from quotes_app.models.models import (
     DBSession,
     SessionLogModel
-)
+    )
+
+from util.json_util import default
+
+from builtins import IndexError
 
 
 def query_validation(query, value):
-    """
-    Function using validate query
+    """Function using validate query
+
     :param query:
     :param value:
     :return: true or false
     """
-    return [q for q in query][value - 1]
+    v = [q for q in query]
+    return v[value - 1]
+
+
+def not_found():
+    """Function not found
+
+    :return: Response 404 Not Found
+    """
+    response = {
+        'status': 404,
+        'message': 'Not Found'
+    }
+    return Response(
+        json_body=response,
+        content_type='application/json; charset=UTF-8',
+        status=404)
 
 
 class RestApiSessionsViewsTests:
@@ -36,35 +53,46 @@ class RestApiSessionsViewsTests:
             return Response(
                 json_body=json.loads(response),
                 content_type='application/json; charset=UTF-8',
-                status=200,
-            )
+                status=200)
         else:
             response = {'sessions': []}
             return Response(
                 json_body=response,
                 content_type='application/json; charset=UTF-8',
-                status=200,
-            )
+                status=200)
 
     @view_config(route_name='api_sessions_id', request_method='GET', renderer='json')
     def api_get_session_id(self):
         get_id = int(self.request.matchdict['id'])
         query = self.session.query(SessionLogModel.id == get_id)
+
         try:
             query_validation(query, get_id)
         except IndexError:
-            response = {'status': 404, 'message': 'Not Found'}
-            return Response(
-                json_body=response,
-                content_type='application/json; charset=UTF-8',
-                status=404,
-            )
+            # List not support len
+            return not_found()
 
-        query_session = self.session.query(SessionLogModel) \
-            .get(get_id).to_json()
+        query_session = self.session.query(SessionLogModel).\
+            get(get_id).to_json()
         response = json.dumps(query_session, default=default)
         return Response(
             json_body=json.loads(response),
             content_type='application/json; charset=UTF-8',
-            status=200,
-        )
+            status=200)
+
+    @view_config(route_name='api_sessions_uid', request_method='GET', renderer='json')
+    def api_get_sessions_uid(self):
+        get_uid = self.request.matchdict['uid']
+
+        query_sessions = self.session.query(SessionLogModel).\
+            filter(SessionLogModel.session_uid == get_uid)
+        serialize = [s.to_json() for s in query_sessions]
+        response = json.dumps(serialize, default=default)
+
+        if len(serialize) > 0:
+            return Response(
+                json_body=json.loads(response),
+                content_type='application/json; charset=UTF-8',
+                status=200)
+        else:
+            return not_found()
