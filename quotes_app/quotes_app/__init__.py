eb752b3b748e20960d035013edfe3549ebe1f57b
fcschmidt <fcschimidt@gmail.com>
2018-01-20 12:11:05 -0200

    Add new hash

--- quotes_app/quotes_app/__init__.py
+++ quotes_app/quotes_app/__init__.py
@@ -15,7 +15,7 @@ def main(global_config, **settings):
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
 
-    session_factory = SignedCookieSessionFactory('itsaseekreet')
+    session_factory = SignedCookieSessionFactory('031edd7d41651593c5fe5c006fa5752b37fddff7bc4e843aa6af0c950f4b9406')
     config.set_session_factory(session_factory)
 
     config.include('pyramid_jinja2')
