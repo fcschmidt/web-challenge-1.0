cd667209400f4bc58a61b279298530312b230ad7
fcschmidt <fcschimidt@gmail.com>
2018-01-19 23:21:37 -0200

    Remove Route not used

--- quotes_app/quotes_app/routes.py
+++ quotes_app/quotes_app/routes.py
@@ -6,7 +6,6 @@ def includeme(config):
     config.add_route('quotes', '/quotes')
     config.add_route('quote', '/quotes/{quote_number}')
     config.add_route('random_quote', '/quotes/random/')
-    config.add_route('sessions', '/sessions/consultas')
     config.add_route('api_sessions', '/api/sessions')
     config.add_route('api_sessions_id', '/api/sessions/{id}')
     config.add_route('api_sessions_uid', '/api/sessions/uid/{uid}')
