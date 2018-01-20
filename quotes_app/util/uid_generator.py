0e5dbfa472118f96aa84f6cd68247ba42c2a40ad
fcschmidt <fcschimidt@gmail.com>
2018-01-20 12:24:27 -0200

    Add description

--- quotes_app/util/uid_generator.py
+++ quotes_app/util/uid_generator.py
@@ -1,9 +1,15 @@
+# This generator can be replaced by lib uuid of python
+# https://docs.python.org/3.5/library/uuid.html
+# or use lib hash https://docs.python.org/3/library/hashlib.html
 import random
 import time
 
 
 def uid_generator():
-    """Uid generator for a session and return it."""
+    """Returns a 40-character uid
+
+    Uid generator for a session and return it.
+    """
     timestamp = "%08x" % time.time()
     uid_session = '%s%s' % (timestamp, "%016x" % random.getrandbits(128))
     return uid_session
