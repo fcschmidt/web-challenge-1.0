a945b9b923d62c0d55d1fe6ecb00edbda314cffd
fcschmidt <fcschimidt@gmail.com>
2018-01-20 12:58:00 -0200

    Update Description

--- quotes_app/util/json_util.py
+++ quotes_app/util/json_util.py
@@ -2,7 +2,10 @@ import datetime
 
 
 def default(obj):
-    """Convert Datetime Python objects to string JSON"""
+    """Convert Datetime Python objects to string JSON
+
+    Return string JSON
+    """
     if type(obj) is datetime.date or type(obj) is datetime.datetime:
         return obj.isoformat()
     if type(obj) is datetime.time or type(obj) is datetime.datetime.time:
