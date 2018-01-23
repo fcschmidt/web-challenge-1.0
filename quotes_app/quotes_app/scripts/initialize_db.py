51aef92ec9c0e06afc522e58949d1be41ecc7629
fcschmidt <fcschimidt@gmail.com>
2018-01-23 13:49:24 -0200

    Code Review

--- quotes_app/quotes_app/scripts/initialize_db.py
+++ quotes_app/quotes_app/scripts/initialize_db.py
@@ -1,15 +1,17 @@
 from sqlalchemy import engine_from_config
-from ..models.models import Base, DBSession
 from pyramid.paster import get_appsettings
+from ..models.models import Base, DBSession
 
 
 def main():
-    """Script used to initialize an empty database"""
+    """Script used to initialize an empty database
+
+    """
     settings = get_appsettings('./development.ini')
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
-    
     Base.metadata.create_all(engine)
+
     return 'Database created successfully!'
 
 
